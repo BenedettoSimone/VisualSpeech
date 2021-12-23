2d83846bf19dcaded2277603057194ca9a7529ae
benedettosimone <sim.benede@gmail.com>
2021-12-23 20:17:55 +0100

    communication ok

--- src/main/webapp/main.py
+++ src/main/webapp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template, make_response
 import webbrowser
 
 app = Flask(__name__)
@@ -9,10 +9,37 @@ def index():
     return "hello"
 
 
-@app.route('/main')
+    #return render_template("http://localhost:8080/VisualSpeech_war_exploded/prova.html", a)
+
+@app.route('/main', methods=['GET','POST'])
 def index1():
+    req = request.get_json()
+    print(req)
 
-    webbrowser.open('https://www.apple.com')
+    res = make_response(jsonify({"message":"JSON received"}), 200)
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE'
 
-if(__name__)=='__main__':
+    return res
+
+
+@app.route("/parameter/<a>")
+def index2(a):
+    return '<h1>' + a + '<h1>'
+
+
+@app.route("/response", methods=['GET', 'POST'])
+def index3():
+    dataGet = request.get_json(force=True)
+    print(dataGet)
+
+    dataReply = {'this': 'that'}
+    return jsonify(dataReply)
+
+
+if (__name__) == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
+
+#https://towardsdatascience.com/talking-to-python-from-javascript-flask-and-the-fetch-api-e0ef3573c451
--- src/main/webapp/prova.js
+++ src/main/webapp/prova.js
@@ -1,11 +1,71 @@
+/*
+var entry= {
+    name: "Daniele",
+    message: "Ciao Daniele"
+}
+
+*/ /*
+str=JSON.stringify(entry)
+
+fetch('http://192.168.1.51:80/main?str='+str, {
+    method: "GET",
+    credentials:"include",
+    //body: ,
+    cache:"no-cache",
+    headers: new Headers({
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Methods" : "GET,PUT,POST,DELETE"
+    })
+})
+    .then(function (response){
+
+        console.log("ciao")
+        response.json().then(function(data){
+            console.log(data)
+        })
+})
+
+*/
+
+
+
+
 $.ajax({
 
     url: 'http://192.168.1.51:80/main',
     //data: null,
     type: 'GET',
-    crossDomain: true,
-    dataType: 'jsonp',
-    success: function() { alert("Success"); },
+    //crossDomain: true,
+    //dataType: 'jsonp',
+    success: function(data) {
+        alert("Success");
+        console.log(data)},
+
     error: function() { alert('Failed!'); },
     //beforeSend: setHeader
 });
+
+
+/*
+var xml = new XMLHttpRequest();
+xml.open("POST", "http://192.168.1.51:80/response", true);
+xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+xml.onload = function (){
+    var dataReply = JSON.parse(this.responseText)
+    alert(dataReply)
+};
+
+xml.setRequestHeader('Access-Control-Allow-Origin', "*")
+xml.setRequestHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+xml.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+
+dataSend = JSON.stringify( {
+    'somedata':'data',
+    'moredata':'moredata'
+});
+
+xml.send(dataSend);
+
+*/
