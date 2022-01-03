ca50ba5ff7c0c7a8d14c617fd2c23acc20549e40
benedettosimone <sim.benede@gmail.com>
2022-01-03 12:09:20 +0100

    make prediction

---
+++ src/main/webapp/Predict.py
@@ -0,0 +1,27 @@
+from keras.preprocessing.image import load_img
+from keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+
+def predict():
+    model = load_model('1_rmsprop.h5', compile=False)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    img = load_img('test.png', color_mode="grayscale", target_size=(224, 224), interpolation="nearest")
+
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_batch = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_batch)
+    prediction_class = np.argmax(prediction, axis=1)
+
+    class_names = ["Stop navigation", "Excuse me", "I am sorry", "Thank you", "Good bye", "I love this grace",
+                   "Nice to meet you", "You are welcome", "How are you", "Have a good time", "Begin", "Choose",
+                   "Connection", "Navigation", "Next", "Previous", "Start", "Stop", "Hello", "Web"]
+    print(class_names[prediction_class[0] - 1])
+    print(prediction_class[0])
+
+    return class_names[prediction_class[0] - 1]
+
